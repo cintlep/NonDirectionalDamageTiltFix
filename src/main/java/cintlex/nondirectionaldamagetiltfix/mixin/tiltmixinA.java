b702914a9305830385b38b5298860e25a17282e6
Trenton <trenturd@outlook.com>
2026-07-30 17:55:11 -0700

    3.0.0 rewrite
    Another internal rewrite for better compatibility and less version specific crashes, functionally the same

---
+++ src/main/java/cintlex/nondirectionaldamagetiltfix/mixin/tiltmixinA.java
@@ -0,0 +1,48 @@
+package cintlex.nondirectionaldamagetiltfix.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(GameRenderer.class)
+public class tiltmixinA {
+
+	@Unique
+	private DamageSource ndtf$ds = null;
+	@Unique
+	private float ndtf$rd = 0.0f;
+
+	@WrapOperation(
+			method = "bobHurt",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/entity/LivingEntity;getHurtDir()F"
+			)
+	)
+	private float ndtf$nondirectionaltilt(LivingEntity le, Operation<Float> original) {
+		if (le.hurtTime <= 0) {
+			return original.call(le);
+		}
+		DamageSource ds = le.getLastDamageSource();
+		if (ds == null || directionaltilt(ds)) {
+			return original.call(le);
+		}
+		if (ndtf$ds != ds) {
+			ndtf$rd = le.getRandom().nextBoolean() ? 0.0f : 180.0f;
+			ndtf$ds = ds;
+		}
+		return ndtf$rd;
+	}
+
+	@Unique
+	private static boolean directionaltilt(DamageSource ds) {
+		return ds.getSourcePosition() != null
+				|| ds.getEntity() != null
+				|| ds.getDirectEntity() != null;
+	}
+}
